609a0b051d49d0fd15e9d9e9ef616304600c78fe
zamanrabeen671 <manbeen671@gmail.com>
2021-05-12 14:26:42 +0600

    status solved

--- src/component/AdminPanel/OrderList/OrderTable.js
+++ src/component/AdminPanel/OrderList/OrderTable.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const OrderTable = ({ bookings }) => {
     console.log(bookings);
-    const handleStatus = (id, name) => {
-        console.log(id);
-        console.log(name);
+    const handleStatus = (status, id) => {
+        console.log('booking id',id);
+        console.log('booking status',status)
 
         fetch(`https://morning-waters-67537.herokuapp.com/update/${id}`,{
             method: 'PATCH',
             headers: {'content-type': 'application/json'},
-            body: JSON.stringify({status: name})
+            body: JSON.stringify({status: status})
         })
         .then(res => res.json())
         .then(data => {
@@ -35,11 +35,11 @@ const OrderTable = ({ bookings }) => {
                         <div className="col-md-2">{booking.service}</div>
                         <div className="col-md-2">Credit Card</div>
                         <div className="col-md-3">
-                            <div>
-                                <button onClick={() => handleStatus(booking._id, 'Pending')}  className="btn btn-danger">Pending</button> <br/>
-                                <button onClick={() => handleStatus(booking._id, 'Done')}  className="btn btn-success">Done</button> <br/>
-                                <button onClick={() => handleStatus(booking._id, 'On Going')}  className="btn btn-info">On Going</button>
-                            </div>
+                            <select onChange= {e=> handleStatus(e.target.value, booking._id)} className={booking.status == 'Pending'? 'btn btn-danger' : booking.status  == 'Done' ? 'btn btn-success' : 'btn btn-info'}>
+                                <option selected = {booking.status == 'Pending'} className="btn btn-danger">Pending</option>
+                                <option selected = {booking.status == 'Done'} className="btn btn-success">Done</option>
+                                <option selected = {booking.status == 'On Going'} className="btn btn-info">On Going</option>
+                            </select>
                         </div>
                     </div>
 
